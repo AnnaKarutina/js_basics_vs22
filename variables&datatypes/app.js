65bcc8113c18c6e76ac7c7b7a6ade9c5dfc61a3c
Anna Karutina <anna.karutina@khk.ee>
2022-09-14 10:57:20 +0300

    Text output and comments

---
+++ variables&datatypes/app.js
@@ -0,0 +1,16 @@
+/**
+ * documentation
+ * comment
+ * style
+ */
+
+// this is simple comment
+
+/*
+console.log("first row")
+console.log("second row")
+console.log("third row") */
+
+console.log("first row")
+console.log("second row")
+console.log("third row")
