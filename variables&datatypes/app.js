53a9eb7563dc44b8a3adcc89e8735f177f3eb139
Anna Karutina <anna.karutina@khk.ee>
2022-09-21 10:26:18 +0300

    Variables and data types - Ex 1.2

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,1 +1,6 @@
-console.log('Tere, maailm!')
+let aasta = 2016
+let president = 'Kersti Kaljulaid'
+let lauseKeskosa = '. aastal valiti Eesti presidendiks '
+let lause = aasta + lauseKeskosa + president
+
+console.log(lause)
