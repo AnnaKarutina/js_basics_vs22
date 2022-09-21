5e384288a2aee24ed1aa0810b5645a12acfc7ec7
Anna Karutina <anna.karutina@khk.ee>
2022-09-21 10:21:13 +0300

    Variables and data types - Ex 1.1

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,18 +1,1 @@
-/**
- * data types
- *
- * primitives - integer, float,
- * objects
- *
- * variables - var, let, const
- *
- * let variableName = value // mutable
- * const constName = value // immutable
- */
-
-let numberOne = 8
-let numberTwo = 2.5
-const season = "Autumn"
-// season = "Spring" ! error
-console.log(numberOne + numberTwo)
-console.log(season + season)
+console.log('Tere, maailm!')
