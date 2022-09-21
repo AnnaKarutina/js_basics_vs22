3e58d0001c223d249e447a69d9e67a64541bec85
Anna Karutina <anna.karutina@khk.ee>
2022-09-21 11:00:35 +0300

    Variables and data types - Ex 1.3 - with comments

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,5 +1,7 @@
+// kasutaja sisend
 let astmeAlus = Number(prompt('Sisestage astme alus'))
 let astendaja = Number(prompt('Sisestage astendaja2'))
-
+// arvutus
 let tulemus = Math.pow(astmeAlus, astendaja)
+// tulemuse väljastamine
 console.log(tulemus)
