afabcbe8f25a91d158073a0a6a85baa2f5ac68c3
Anna Karutina <anna.karutina@khk.ee>
2022-09-22 15:17:25 +0300

    Conditional statements - Ex 2.3

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,5 +1,10 @@
 // == != > < >= <= - võrdlusoperaatorid -> true / false
 
+// loogilised operaatorid
+// AND - &&
+// OR - ||
+// NOT - !
+
 /*
 if(tingimus){
     tegevused, mis sooritatakse, kui
@@ -10,11 +15,42 @@ if(tingimus){
 }
  */
 
-// kasutaja sisend
-let temp = Number(prompt('Sisesta õhutemperatuur'))
-// kontroll ja väljastus
-if(temp > 4.0){
-    console.log('Ei ole jäätumise ohtu')
+/*
+if(tingimus1){
+    kui see kehtib, siis edasi pole vaja kontrollida
+    kui aga mitte, vaadatakse järgmine tinfimus
+} else if(tingimus2){
+    kui see kehtib, siis edasi pole vaja kontrollida
+    kui aga mitte, vaadatakse järgmine tinfimus
 } else {
-    console.log('On jäätumise oht')
+    täidetakse siis, kui kõik eelmised ei sobinud
 }
+*/
+
+// kasutaja sisend
+let vanus = Number(prompt('Sisesta oma vanus'))
+let sugu = prompt('Sisesta oma sugu')
+let treening = Number(prompt('Sisesta treeningu tüüp'))
+// kontroll ja väljastus
+let pulsisagedus;
+
+if(sugu == 'm' || sugu == 'M'){
+    pulsisagedus = 220 - vanus
+} else if(sugu == 'n' || sugu == 'N'){
+    pulsisagedus = 206 - 0.88 * vanus
+}
+let minPulsisagedus;
+let maxPulsisagedus;
+
+if(treening == 1){
+    minPulsisagedus = 0.5 * pulsisagedus
+    maxPulsisagedus = 0.7 * pulsisagedus
+} else if(treening == 2){
+    minPulsisagedus = 0.7 * pulsisagedus
+    maxPulsisagedus = 0.8 * pulsisagedus
+} else if(treening == 3) {
+    minPulsisagedus = 0.8 * pulsisagedus
+    maxPulsisagedus = 0.87 * pulsisagedus
+}
+
+console.log(`Pulsisagedis peab olema vahemikus ${minPulsisagedus} kuni ${maxPulsisagedus}`)
