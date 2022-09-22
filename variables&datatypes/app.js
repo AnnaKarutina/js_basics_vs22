f76b34dbca47ea0902684fae34af592f1108046c
Anna Karutina <anna.karutina@khk.ee>
2022-09-22 14:37:20 +0300

    Variables and data types - Ex 1.6 - with if condition

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -6,4 +6,19 @@ let kohtiBussis = 50
 let bussidTäidetud = Math.floor(inimesed / kohtiBussis)
 let mahaJaanud = inimesed % kohtiBussis
 // tulemuse väljastamine
-console.log(inimesed + ' inimeste sõidutamiseks läheb vaja ' + bussidTäidetud + ' bussi ja ' + mahaJaanud + ' inimest on jäänud maha')
+console.log(inimesed + ' inimeste sõidutamiseks läheb vaja ' + bussidTäidetud + ' bussi')
+if(mahaJaanud != 0){
+    console.log('ja ' + mahaJaanud + ' inimest on jäänud maha')
+}
+
+// == != > < >= <= - võrdlusoperaatorid -> true / false
+
+/*
+if(tingimus){
+    tegevused, mis sooritatakse, kui
+    vastus tingimusele on true
+} else {
+    tegevused, mis sooritatakse, kui
+    vastus tingimusele on false
+}
+ */
