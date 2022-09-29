a0ec0c4be7209c6729697c272745da4541fc7191
Anna Karutina <anna.karutina@khk.ee>
2022-09-29 12:33:10 +0300

    Loops - Ex 3.1

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,28 +1,10 @@
-// for
+// kasutaja sisend
+let kordamise_arv = Number(prompt('Sisestage mitu korda äratada'))
 
-/*
-for (let nimi = algvaartus; tingimus; suurendamine/vahendamine){
-    tegevused, mis sooritatakse korduvalt
-}
-
-while(tingimus){
-    tegevused, mis sooritatakse korduvalt
-}
-*/
-
-for(let number = 1; number <= 10; number++){
-    if(number == 5){
-        // console.log(number)
-        // break;
-        continue;
-    }
-    console.log(number)
-}
-console.log('for tsükli töö lõpp')
-
-let kord = 1
-while(kord <= 10){
-    console.log(kord)
+let kord = 1 // tsükli juhtimismuutuja algväärtus
+// korduvad tegevused
+while(kord <= kordamise_arv){
+    console.log(`Tõuse ja sära - ${kord}. kord`)
     kord++
 }
-console.log('while tsükli lõpp')
+console.log('Programmi lõpp')
